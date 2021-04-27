73aae8e6039645b582bcfea88e781010900f357e
AagashUni21 <>
2021-04-27 17:04:17 +0000

    ele2

---
+++ meu.py/task 2 piu.py
@@ -0,0 +1,16 @@
+print("Please choose an option from the menu:\n\nLoad Data\n\nProcess Data\n"
+          "\nVisualise Data\n\nSave Data\n\nExit\n")
+    option = input()
+
+    if option == "Load Data":
+        print("\n1")
+    elif option == "Process Data":
+        print("\n2")
+    elif option == "Visualise Data":
+        print("\n3")
+    elif option == "Save Data":
+        print("\n4")
+    elif option == "Exit":
+        print("\n5")
+    else:
+        print("\nNone")

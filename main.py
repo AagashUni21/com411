c6d769dcbdf2f71d05a184e2853339836de06a28
AagashUni21 <>
2021-05-04 09:50:10 +0000

    subplots

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+def completed(operation):
+    """
+    Task 4: Display a message to indicate that an operation has completed.
 
+    The function should display a message in the following format:
+    '{operation} has completed.'
+    Where {operation} is the value of the parameter passed to this function
 
+    :param operation: A string indicating the operation being completed
+    :return: Does not return anything
+    """
+    # TODO: Your code here
 
